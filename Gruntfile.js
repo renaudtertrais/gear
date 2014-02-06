91ecff829f25db5dff78cd0a88bcbb0ccfa6f347
eMaj-Fr <renaud.tertrais@gmail.com>
2014-02-07 00:23:23 +0100

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,8 +36,6 @@ module.exports = function(grunt){
 	grunt.task.registerTask("dist-css", ["sass"]);
 
 	// * * * Grunt Plugins * * *
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 }
-
-
-grunt.loadNpmTasks('grunt-contrib-watch');
